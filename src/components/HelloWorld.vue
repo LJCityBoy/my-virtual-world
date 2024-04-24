e35a62aceaf3b53ac860d005a3cff5329bac75f6
LJCityBoy <1334417517@qq.com>
2024-04-25 00:23:52 +0800

    初始化cesium

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,34 @@
-<script setup lang="ts">
-import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div id="cesium-container"></div>
 </template>
 
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium'
+
+onMounted(() => {
+  //配置token
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhYjQ4Mzk5NS03OGYzLTQ1NGItODM3ZC1lNTI4NTUyZjllYWMiLCJpZCI6MTEyNzYsImlhdCI6MTcxMzk3MzgxMH0.-YBx86oMJLA1SKl1yvWVetk2KsQw65N9Thi048Xly0E'
+  //初始化
+  const viewer = new Cesium.Viewer('cesium-container', { terrain: Cesium.Terrain.fromWorldTerrain() })
+
+  viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+    orientation: {
+      heading: Cesium.Math.toRadians(0.0),
+      pitch: Cesium.Math.toRadians(-15.0),
+    }
+  })
+})
+
+
+</script>
+
 <style scoped>
-.read-the-docs {
-  color: #888;
+.cesium-container {
+  height: 100%;
+  width: 100%;
 }
 </style>
